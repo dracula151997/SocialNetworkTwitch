6609867ed796d2a370a6af9a75d968a93f0215b6
Hassan Mohammed <Hassan.Mohamed@advansys-esc.com>
2023-05-03 05:27:50 +0300

    Created ReadMoreText composable

--- app/src/main/java/com/dracula/socialnetworktwitch/core/presentation/components/ReadMoreText.kt
+++ app/src/main/java/com/dracula/socialnetworktwitch/core/presentation/components/ReadMoreText.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
@@ -25,30 +27,66 @@ fun ReadMoreText(
     text: String,
     modifier: Modifier = Modifier,
     maxLines: Int = Int.MAX_VALUE,
-    readMoreText: String = stringResource(id = R.string.read_more),
     readLessText: String = stringResource(id = R.string.read_less),
     readMoreTextColor: Color = MaterialTheme.colors.primary,
 ) {
     val isExpanded = remember {
         mutableStateOf(false)
     }
+    val textLayoutResultState = remember { mutableStateOf<TextLayoutResult?>(null) }
+    val finalText = remember { mutableStateOf(text) }
+    val readMoreText = remember {
+        mutableStateOf("Read More")
+    }
+
+    val textLayoutResult = textLayoutResultState.value
+    LaunchedEffect(key1 = textLayoutResult) {
+        if (textLayoutResult == null) return@LaunchedEffect
+
+        when {
+            isExpanded.value -> {
+                readMoreText.value = "Read less"
+            }
+
+            !isExpanded.value && textLayoutResult.hasVisualOverflow -> {
+                readMoreText.value = "Read more"
+                val lastCharIndex = textLayoutResult.getLineEnd(maxLines - 1)
+                val adjustedText = text.substring(startIndex = 0, endIndex = lastCharIndex)
+                    .dropLast(readMoreText.value.length)
+                    .dropLastWhile { it == ' ' || it == '.' }
+
+                finalText.value = adjustedText
+            }
+
+            else -> {
+                finalText.value = text
+                readMoreText.value = ""
+            }
+        }
+    }
 
     Column(
         modifier = Modifier.animateContentSize()
     ) {
         Text(
-            text = text,
+            text = finalText.value,
             maxLines = if (isExpanded.value) Int.MAX_VALUE else maxLines,
             overflow = TextOverflow.Ellipsis,
-            modifier = modifier
+            modifier = modifier,
+            onTextLayout = {
+                textLayoutResultState.value = it
+            }
+
         )
+
+
         ClickableText(
             text = buildAnnotatedString {
-                append(if (!isExpanded.value) readMoreText else readLessText)
+                append(readMoreText.value)
                 addStyle(
                     style = SpanStyle(
                         color = readMoreTextColor, textDecoration = TextDecoration.Underline
-                    ), start = 0, end = readMoreText.length
+                    ), start = 0, end = readMoreText.value.length
                 )
             },
             onClick = {
@@ -66,7 +104,6 @@ fun ReadMoreTextPreview() {
         ReadMoreText(
             text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae elit arcu. Maecenas tincidunt aliquet sapien non laoreet. Quisque dignissim molestie justo, sit amet consequat sem blandit in. Nulla ac fringilla leo. Nunc eget orci quis ipsum aliquam posuere. Aliquam vestibulum ex sit amet velit malesuada, sit amet fringilla arcu facilisis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque sem ex, efficitur et massa non, gravida dictum libero. Etiam sit amet mauris et tortor lacinia volutpat. In sit amet sapien massa. Maecenas quis neque nec felis efficitur molestie. Donec sodales erat at pharetra mollis. Proin placerat elementum lacus, vel interdum metus molestie et. Aliquam lacinia tellus vel volutpat fermentum. Vivamus laoreet quam nec ante lacinia, eget semper ipsum lacinia.",
             maxLines = Int.MAX_VALUE,
-            readMoreText = stringResource(id = R.string.read_more),
             readMoreTextColor = MaterialTheme.colors.primary
         )
     }
